06066ad5c3f9fdb533db43d927dd3343457a2eb1
MarkSoyiri <marksoyiri001@gmail.com>
2025-08-11 19:41:35 +0100

    Help Page 90% Complete

--- js/index.js
+++ js/index.js
@@ -54,24 +54,24 @@ helpB.addEventListener("click",function(){
 })
 // PRODUCT SLIDER
 
-// const productsBox = [...document.querySelectorAll('.product-box')];
-// const previousBtn = [...document.querySelectorAll('.previous-btn')];
-// const nextBtn = [...document.querySelectorAll('.next-btn')];
+const productsBox = [...document.querySelectorAll('.product-box')];
+const previousBtn = [...document.querySelectorAll('.previous-btn')];
+const nextBtn = [...document.querySelectorAll('.next-btn')];
 
-// productsBox.forEach((items,i)=>{
-//     let containerDimensions = items.getBoundingClientRect();
-//     let containerWidth = containerDimensions.width;
-//     nextBtn[i].addEventListener('click',()=>{
-//         items.scrollLeft += containerWidth;
+productsBox.forEach((items,i)=>{
+    let containerDimensions = items.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+    nextBtn[i].addEventListener('click',()=>{
+        items.scrollLeft += containerWidth;
         
-//     })
+    })
 
-//     previousBtn[i].addEventListener('click',()=>{
-//         items.scrollLeft -= containerWidth;
+    previousBtn[i].addEventListener('click',()=>{
+        items.scrollLeft -= containerWidth;
         
         
-//     })
+    })
     
-// })
+})
 
   
